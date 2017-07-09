94d25525090f9fd36f229d110b6706dc86c1f0d0
Sean Kelly <theseankelly@outlook.com>
2017-07-09 07:41:58 -0700

    Removing throttle on orientation estimation

--- src/modules/src/estimator_complementary.c
+++ src/modules/src/estimator_complementary.c
@@ -42,18 +42,16 @@ void stateEstimator(state_t *state, const sensorData_t *sensorData, const uint32
   lastUpdateUs = timestamp;
 #endif
 
-  if (RATE_DO_EXECUTE(ATTITUDE_UPDATE_RATE, tick)) {
-    sensfusion6UpdateQ(sensorData->gyro.x, sensorData->gyro.y, sensorData->gyro.z,
-                       sensorData->acc.x, sensorData->acc.y, sensorData->acc.z,
-                       dt);
-    sensfusion6GetEulerRPY(&state->attitude.roll, &state->attitude.pitch, &state->attitude.yaw);
+  sensfusion6UpdateQ(sensorData->gyro.x, sensorData->gyro.y, sensorData->gyro.z,
+                     sensorData->acc.x, sensorData->acc.y, sensorData->acc.z,
+                     dt);
+  sensfusion6GetEulerRPY(&state->attitude.roll, &state->attitude.pitch, &state->attitude.yaw);
 
-    state->acc.z = sensfusion6GetAccZWithoutGravity(sensorData->acc.x,
-                                                    sensorData->acc.y,
-                                                    sensorData->acc.z);
+  state->acc.z = sensfusion6GetAccZWithoutGravity(sensorData->acc.x,
+                                                  sensorData->acc.y,
+                                                  sensorData->acc.z);
 
-    positionUpdateVelocity(state->acc.z, ATTITUDE_UPDATE_DT);
-  }
+  positionUpdateVelocity(state->acc.z, ATTITUDE_UPDATE_DT);
 
   if (RATE_DO_EXECUTE(POS_UPDATE_RATE, tick)) {
     // If position sensor data is preset, pass it throught
