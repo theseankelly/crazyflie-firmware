129927cbcf303d2838ece117c94eafb15689bca5
Sean Kelly <theseankelly@outlook.com>
2017-07-09 07:38:11 -0700

    Changing compl estimator to use real dt
    Using computed dt instead of constant 250hz

--- src/modules/src/estimator_complementary.c
+++ src/modules/src/estimator_complementary.c
@@ -5,14 +5,19 @@
 #include "sensfusion6.h"
 #include "position_estimator.h"
 
+#include "usec_time.h"
+
 #define ATTITUDE_UPDATE_RATE RATE_250_HZ
 #define ATTITUDE_UPDATE_DT 1.0/ATTITUDE_UPDATE_RATE
 
 #define POS_UPDATE_RATE RATE_100_HZ
 #define POS_UPDATE_DT 1.0/POS_UPDATE_RATE
 
+static uint64_t lastUpdateUs = 0;
+
 void stateEstimatorInit(void)
 {
+  lastUpdateUs = usecTimestamp();
   sensfusion6Init();
 }
 
@@ -27,10 +32,20 @@ bool stateEstimatorTest(void)
 
 void stateEstimator(state_t *state, const sensorData_t *sensorData, const uint32_t tick)
 {
+  // Compute dt
+#if 1 // Use sensor data timestamp for basis of calculation
+  float dt = ((float)(sensorData->gyro.timestamp - lastUpdateUs))/1000000.0f;
+  lastUpdateUs = sensorData->gyro.timestamp;
+#else // use t_now for basis of caluclation
+  uint64_t timestamp = usecTimestamp();
+  float dt = ((float)(timestamp - lastUpdateUs))/1000000.0f;
+  lastUpdateUs = timestamp;
+#endif
+
   if (RATE_DO_EXECUTE(ATTITUDE_UPDATE_RATE, tick)) {
     sensfusion6UpdateQ(sensorData->gyro.x, sensorData->gyro.y, sensorData->gyro.z,
                        sensorData->acc.x, sensorData->acc.y, sensorData->acc.z,
-                       ATTITUDE_UPDATE_DT);
+                       dt);
     sensfusion6GetEulerRPY(&state->attitude.roll, &state->attitude.pitch, &state->attitude.yaw);
 
     state->acc.z = sensfusion6GetAccZWithoutGravity(sensorData->acc.x,
